36d75d677a3e1ccc7a7a2d6ba54aef698baa921b
raoneinet <raoneinet@hotmail.com>
2025-12-08 12:41:20 +0000

    feat: adapt headers

--- api/headers.php
+++ api/headers.php
@@ -1,4 +1,10 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: PUT, POST, GET, OPTIONS, DELETE");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+if ($_SERVER["REQUEST_METHOD"] === "OPTIONS") {
+    http_response_code(204);
+    exit();
+}
 header("Content-Type: application/json");
